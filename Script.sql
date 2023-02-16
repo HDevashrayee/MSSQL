7a20faadfee7cca5df94c264f8495a4a447ee8e7
HDevashrayee <hitesh.devashrayee@outlook.com>
2023-02-16 21:34:31 +0530

    Commit

--- Script.sql
+++ Script.sql
@@ -9,3 +9,12 @@ alter table Citylist alter column statecode nvarchar(50) null;
 -- Drop Fk
 
 --FK_CityList_CountryList_CountryCode
+
+--Create database Development
+
+Use Development
+
+create table HumanResources(
+FirstName varchar(50),
+LastName varchar(50),
+Department varchar(50))
