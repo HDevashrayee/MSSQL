227fe8b4c6d49feb2918d085095162d6cb22bcb2
HDevashrayee <113212932+HDevashrayee@users.noreply.github.com>
2023-03-06 16:48:43 +0530

    commit

--- Script.sql
+++ Script.sql
@@ -2,9 +2,21 @@
 alter table Citylist alter column statecode nvarchar(50) null;
 
 --Drop index
+Drop Index IX_CityList_StateCode on Citylist
 
+--Create Index
+Create Index IX_CityList_StateCode on Citylist (statecode)
+
+--Drop FK
+Alter table citylist drop constraint FK_CityList_StateList_StateCode
+
+--Add FK
+
+Alter table citylist add constraint FK_CityList_StateList_StateCode foreign key (StateCode) references StateList(StateCode)
 
 --IX_CityList_StateCode
+
+
 
 -- Drop Fk
 
